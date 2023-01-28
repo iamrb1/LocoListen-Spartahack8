75d08258b7fa22f8bbb7b69bf4d111cdf2cb231f
hemanthy <hemanthy@gmail.com>
2023-01-28 18:29:07 -0500

    lol

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,10 @@
 # streamlit_app.py
 
 import streamlit as st
-from google.oauth2 import service_account
 from gsheetsdb import connect
 
 # Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+conn = connect()
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
@@ -21,7 +14,7 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
+sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
