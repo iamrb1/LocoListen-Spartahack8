0cb76130ae6b0e2720eb3245dc4661b5a54fba55
hemanthy <hemanthy@gmail.com>
2023-01-28 19:21:58 -0500

    lol

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,14 @@
 import streamlit as st
+import json
+import requests
 
+st.title("Get User Location")
 
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-
-loc_button = Button(label="Get Location")
-loc_button.js_on_event("button_click", CustomJS(code="""
-    navigator.geolocation.getCurrentPosition(
-        (loc) => {
-            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-        }
-    )
-    """))
-result = streamlit_bokeh_events(
-    loc_button,
-    events="GET_LOCATION",
-    key="get_location",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-
-'''
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
-    '''
+if st.button("Get Location"):
+    response = requests.get("http://ip-api.com/json")
+    data = json.loads(response.text)
+    st.write("City: ", data["city"])
+    st.write("Region: ", data["regionName"])
+    st.write("Country: ", data["country"])
+    st.write("Latitude: ", data["lat"])
+    st.write("Longitude: ", data["lon"])
