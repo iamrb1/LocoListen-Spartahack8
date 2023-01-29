803d30c10601f68012e043277413ea6e35b22a54
hemanthy <hemanthy@gmail.com>
2023-01-28 20:20:50 -0500

    lol

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import json
 import requests
 
-st.title("Get User Location")
 
-if st.button("Get Location"):
-    response = requests.get("http://ip-api.com/json")
-    data = json.loads(response.text)
-    st.write("City: ", data["city"])
-    st.write("Region: ", data["regionName"])
-    st.write("Country: ", data["country"])
-    st.write("Latitude: ", data["lat"])
-    st.write("Longitude: ", data["lon"])
+
+
+response = requests.get("http://ip-api.com/json")
+data = json.loads(response.text)
+st.write("City: ", data["city"])
+st.write("Region: ", data["regionName"])
+st.write("Country: ", data["country"])
+st.write("Latitude: ", data["lat"])
+st.write("Longitude: ", data["lon"])
