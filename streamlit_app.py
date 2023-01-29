8c7349d5de973e99f0d48d2d5d203c1672311316
hemanthy <hemanthy@gmail.com>
2023-01-28 19:13:06 -0500

    lol

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,28 @@
 import streamlit as st
 
 
-# Create a connection object.
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: rgb(204, 49, 49);
-}
-</style>""", unsafe_allow_html=True)
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-b = st.button("test")
-
+loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
+
 '''
 @st.cache(ttl=600)
 def run_query(query):
