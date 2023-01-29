0724ca033b43a8f807e33b0673d6170c63f41f27
hemanthy <hemanthy@gmail.com>
2023-01-29 00:16:22 -0500

    lol

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,24 @@ import streamlit as st
 import json
 import requests
 import datetime
+from gsheetsdb import connect
 
+# Create a connection object.
+conn = connect()
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
+sheet1 = st.secrets['Sheet1']
+
+rows = run_query(f'SELECT * FROM "{sheet1}"')
+
+# Print results
 
 response = requests.get("http://ip-api.com/json")
 data = json.loads(response.text)
